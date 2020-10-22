faa1eddc7dd081b737be85dd08055735ad3c59fe
Yaukamp <ylanaauk@gmail.com>
2020-10-22 16:30:52 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+// Kata 01 Add
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// console.log(add(2, 4));
+
+// Kata 02 Multiply
+
+// function multiply(a, b) {
+//   let count = 0;
+//   let result = 0;
+//   while (count < b) {
+// result += (a, 0);
+// console.log(result);
+// count++;
+//   }
+//   return result;
+// }
+
+// console.log(multiply(4, 4));
+
+// Kata 03 Power/Exponentiation
+
+function power(x, n) {}
+let count = 0;
+let result = 0;
