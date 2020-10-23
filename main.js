83e9ec5501585a31958756a5bdf4002d354be99a
Yaukamp <ylanaauk@gmail.com>
2020-10-23 17:44:47 -0400

    final commit

--- main.js
+++ main.js
@@ -1,28 +1,54 @@
 // Kata 01 Add
 
-// function add(a, b) {
-//   return a + b;
-// }
+function add(a, b) {
+  return a + b;
+}
 
-// console.log(add(2, 4));
+console.log(add(2, 4));
 
 // Kata 02 Multiply
 
-// function multiply(a, b) {
-//   let count = 0;
-//   let result = 0;
-//   while (count < b) {
-// result += (a, 0);
-// console.log(result);
-// count++;
-//   }
-//   return result;
-// }
+function multiply(a, b) {
+  let count = 0;
+  let result = 0;
+  while (count < b) {
+    result = add(result, a);
+    // console.log(result);
+    count++;
+  }
+  return result;
+}
 
-// console.log(multiply(4, 4));
+console.log(multiply(6, 4));
 
 // Kata 03 Power/Exponentiation
 
-function power(x, n) {}
-let count = 0;
-let result = 0;
+function power(x, n) {
+  let count = 0;
+  let result = 1;
+  while (count < n) {
+    result = multiply(result, x);
+    count++;
+  }
+  return result;
+}
+
+console.log(power(2, 8));
+
+// Kata 04 Factorial
+
+function factorial(n) {
+  let count = 1;
+  let result = 1;
+  while (count <= n) {
+    result = multiply(result, count);
+
+    // console.log(count);
+    // console.log(result);
+    // console.log(n);
+    count++;
+  }
+  return result;
+}
+
+console.log(factorial(4));
